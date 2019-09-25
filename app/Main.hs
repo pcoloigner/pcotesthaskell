168a053b0d5c420f5c24b38a7cda45af54f9655c
pcoloigner <philippe.coloigner@free.fr>
2019-09-25 04:45:26 +0200

    Test autre application avec Spock

--- app/Main.hs
+++ app/Main.hs
@@ -1,34 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import qualified Data.Text as T
-import qualified Data.Text.Lazy as L
-import System.Environment (getEnv)
-import Web.Scotty (get, html, scotty)
+module Main where
 
-import Control.Monad.Trans (liftIO)
-import qualified Data.ByteString.Char8 as B8
-import qualified Database.PostgreSQL.Simple as P
+import Web.Spock
+import Web.Spock.Config
+import Web.Spock.Lucid (lucid)
+import Lucid 
 
-type DbTitle = (T.Text, T.Text)
 
-formatTitles :: [DbTitle] -> T.Text
-formatTitles = T.intercalate "<br/>\n" . map (\ (a, t) -> T.concat [a, " - ", t]) 
-
-getMusic :: String -> IO T.Text
-getMusic params = do
-    conn <- P.connectPostgreSQL $ B8.pack params
-    titles <- P.query_ conn "SELECT artists.name, titles.name FROM artists \
-                            \ INNER JOIN titles ON artists.id = titles.artist" :: IO [DbTitle]
-    P.close conn
-    return $ formatTitles titles
+app :: SpockM () () () ()
+app = get root $ lucid $ do
+    h1_ "Hello"
+    p_ "How are you today"
 
 main :: IO ()
 main = do
-    dbParams <- getEnv "DATABASE_URL"
-    portStr <- getEnv "PORT"
-    let port = read portStr
-
-    scotty port $ do
-        get "/" $ do
-            music <- liftIO $ getMusic dbParams
-            html $ L.fromStrict $ music
+    cfg <- defaultSpockCfg () PCNoDatabase ()
+    runSpock 8080 (spock cfg app)
